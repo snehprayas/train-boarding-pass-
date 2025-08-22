1bfbdac8562ad3c33f99dff816adf6bce40a1f7d
Snehprayas01 <sanyamjain.jmt@gmail.com>
2025-08-22 19:14:36 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+const bookings = {
+  "SPC101": {
+    name: "Sulekha Patni",
+    phone: "7439519345",
+    seat: "23",
+    coach: "B-2",
+    train: "123456",
+    date: "25/08/2025",
+    time: "11:00 PM",
+    from: "HOWRAH",
+    to: "NEW DELHI"
+  },
+  "SPC102": {
+    name: "Abhay Patni",
+    phone: "9903992010",
+    seat: "44",
+    coach: "C-2",
+    train: "234561",
+    date: "25/08/2025",
+    time: "11:00 PM",
+    from: "HOWRAH",
+    to: "NEW DELHI"
+  },
+  "SPC103": {
+    name: "Kathanshi Patni",
+    phone: "6290394857",
+    seat: "10",
+    coach: "B-1",
+    train: "345612",
+    date: "25/08/2025",
+    time: "11:00 PM",
+    from: "HOWRAH",
+    to: "NEW DELHI"
+  }
+};
